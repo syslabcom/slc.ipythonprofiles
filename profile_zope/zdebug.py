93f4a3c35418e607514c573534d13bfe5f45bbea
Cillian de Róiste <goibhniu@fsfe.org>
2014-04-24 15:26:59 +0200

    Update zdebug.py to work with ipython 2.0.0
    copied from https://github.com/collective/dotipython/ipy_profile_zope.py

--- profile_zope/zdebug.py
+++ profile_zope/zdebug.py
@@ -1,28 +1,31 @@
 # -*- coding: utf-8 -*-
-""" An ipython profile for zope and plone.
-
-Some ideas stolen from http://www.tomster.org.
-Also from http://comments.gmane.org/gmane.comp.web.zope.plone.product-developers/6328 for ipython 0.11.
-
-
-Authors
--------
-- Stefan Eletzhofer <stefan.eletzhofer@inquant.de>
-- Manuel Reinhardt <reinhardt@syslab.com>
-"""
-
+#
 # File: zdebug.py (formerly ipy_profile_zope.py)
 #
 # Copyright (c) InQuant GmbH
 #
+# An ipython profile for zope and plone. Some ideas
+# stolen from http://www.tomster.org.
 #
-#  Distributed under the terms of the BSD License.  The full license is in
-#  the file COPYING, distributed as part of this software.
+# German Free Software License (D-FSL)
+#
+# This Program may be used by anyone in accordance with the terms of the
+# German Free Software License
+# The License may be obtained under <http://www.d-fsl.org>.
 
+__author__ = """Stefan Eletzhofer <stefan.eletzhofer@inquant.de>"""
+__docformat__ = 'plaintext'
+__revision__ = "$Revision$"
+
+# IPython.ipapi was moved and then deprecated
 try:
-    from IPython import ipapi
-except:
-    from IPython.core import ipapi
+    # IPython <= 0.10
+    from IPython.ipapi import get as get_inst
+except ImportError:
+    # IPython >= 0.11
+    from IPython.core.interactiveshell import InteractiveShell
+    get_inst = InteractiveShell.instance
+
 from types import StringType
 import sys
 import os
@@ -33,6 +36,8 @@ import textwrap
 # import ipy_defaults
 
 _marker = []
+
+
 def shasattr(obj, attr, acquire=False):
     """ See Archetypes/utils.py
     """
@@ -40,31 +45,30 @@ def shasattr(obj, attr, acquire=False):
         obj = obj.aq_base
     return getattr(obj, attr, _marker) is not _marker
 
+
 class ZopeDebug(object):
     def __init__(self):
 
-        self.instancehome = os.environ.get( "INSTANCE_HOME" )
+        self.instancehome = os.environ.get("INSTANCE_HOME")
 
-        configfile = os.environ.get( "CONFIG_FILE" )
+        configfile = os.environ.get("CONFIG_FILE")
         if configfile is None and self.instancehome is not None:
-            configfile = os.path.join( self.instancehome, "etc", "zope.conf" )
+            configfile = os.path.join(self.instancehome, "etc", "zope.conf")
 
         if configfile is None:
-            raise RuntimeError( "CONFIG_FILE env not set" )
+            raise RuntimeError("CONFIG_FILE env not set")
 
         print "CONFIG_FILE=", configfile
         print "INSTANCE_HOME=", self.instancehome
 
         self.configfile = configfile
 
-        self.portal_path = os.environ.get( "PORTAL_PATH" )
-
         try:
             from Zope2 import configure
         except ImportError:
             from Zope import configure
 
-        configure( configfile )
+        configure(configfile)
 
         try:
             import Zope2
@@ -74,7 +78,7 @@ class ZopeDebug(object):
             app = Zope.app()
 
         from Testing.makerequest import makerequest
-        self.app = makerequest( app )
+        self.app = makerequest(app)
 
         try:
             self._make_permissive()
@@ -87,10 +91,13 @@ class ZopeDebug(object):
         try:
             from zope.component import getSiteManager
             from zope.component import getGlobalSiteManager
-            from zope.app.component.hooks import setSite
+            try:
+                from zope.app.component.hooks import setSite
+            except ImportError:
+                from zope.component.hooks import setSite
 
             if self.portal is not None:
-                setSite( self.portal )
+                setSite(self.portal)
 
                 gsm = getGlobalSiteManager()
                 sm = getSiteManager()
@@ -98,8 +105,8 @@ class ZopeDebug(object):
                 if sm is gsm:
                     print "ERROR SETTING SITE!"
         except:
+            # XXX: What exceptions is this supposed to catch?
             pass
-
 
     @property
     def utils(self):
@@ -121,24 +128,15 @@ class ZopeDebug(object):
 
     @property
     def namespace(self):
-        return dict( utils=self.utils, app=self.app, portal=self.portal )
+        return dict(utils=self.utils, app=self.app, portal=self.portal)
 
     @property
     def portal(self):
-        if self.portal_path:
-            try:
-                portal = self.app.unrestrictedTraverse(self.portal_path)
-            except (KeyError, AttributeError):
-                pass
-            else:
-                if portal.meta_type == 'Plone Site':
-                    return portal
-            print "No Plone Site found at PORTAL_PATH=%s" % self.portal_path
-        portals = self.app.objectValues( "Plone Site" )
+        portals = self.app.objectValues("Plone Site")
         if len(portals):
             return portals[0]
         else:
-            raise KeyError( "No Plone Site found.")
+            return None
 
     def pwd(self):
         return self._pwd
@@ -148,7 +146,8 @@ class ZopeDebug(object):
         Make a permissive security manager with all rights. Hell,
         we're developers, aren't we? Security is for whimps. :)
         """
-        from Products.CMFCore.tests.base.security import PermissiveSecurityPolicy
+        from Products.CMFCore.tests.base.security import (
+            PermissiveSecurityPolicy)
         import AccessControl
         from AccessControl.SecurityManagement import newSecurityManager
         from AccessControl.SecurityManager import setSecurityPolicy
@@ -157,28 +156,37 @@ class ZopeDebug(object):
         self.oldpolicy = setSecurityPolicy(_policy)
         newSecurityManager(None, AccessControl.User.system)
 
-    def su(self, username):
-        """ Change to named user.
+    def su(self, username=None):
+        """Change to named user. Return to permissive security
+        policy if no username is given.
         """
-        # TODO Make it easy to change back to permissive security.
-        user = self.portal.acl_users.getUser(username)
+        if username is None:
+            self._make_permissive()
+            print "PermissiveSecurityPolicy put back in place"
+            return
+
+        user = (
+            self.portal.acl_users.getUser(username) or
+            self.app.acl_users.getUser(username)
+        )
         if not user:
             print "Can't find %s in %s" % (username, self.portal.acl_users)
             return
 
-        from AccessControl import ZopeSecurityPolicy
-        import AccessControl
-        from AccessControl.SecurityManagement import newSecurityManager, getSecurityManager
+        from AccessControl.ZopeSecurityPolicy import ZopeSecurityPolicy
+        from AccessControl.SecurityManagement import newSecurityManager
+        from AccessControl.SecurityManagement import getSecurityManager
         from AccessControl.SecurityManager import setSecurityPolicy
 
-        _policy = ZopeSecurityPolicy
+        _policy = ZopeSecurityPolicy()
         self.oldpolicy = setSecurityPolicy(_policy)
         wrapped_user = user.__of__(self.portal.acl_users)
-        newSecurityManager(None, user)
+        newSecurityManager(None, wrapped_user)
         print 'User changed.'
         return getSecurityManager().getUser()
 
-    def getCatalogInfo(self, obj=None, catalog='portal_catalog', query=None, sort_on='created', sort_order='reverse' ):
+    def getCatalogInfo(self, obj=None, catalog='portal_catalog', query=None,
+                       sort_on='created', sort_order='reverse'):
         """ Inspect portal_catalog. Pass an object or object id for a
         default query on that object, or pass an explicit query.
         """
@@ -193,13 +201,13 @@ class ZopeDebug(object):
         if not query:
             if type(obj) is StringType:
                 cwd = self.pwd()
-                obj = cwd.unrestrictedTraverse( obj )
+                obj = cwd.unrestrictedTraverse(obj)
             # If the default in the signature is mutable, its value will
             # persist across invocations.
             query = {}
             if indexes.get('path'):
                 from string import join
-                path = join(obj.getPhysicalPath(), '/') 
+                path = join(obj.getPhysicalPath(), '/')
                 query.update({'path': path})
             if indexes.get('getID'):
                 query.update({'getID': obj.id, })
@@ -243,21 +251,21 @@ class ZopeDebug(object):
         """
         self.app._p_jar.sync()
 
-    def objectInfo( self, o ):
+    def objectInfo(self, o):
         """
         Return a descriptive string of an object
         """
         Title = ""
-        t = getattr( o, 'Title', None )
+        t = getattr(o, 'Title', None)
         if t:
             Title = t()
         return {'id': o.getId(),
                 'Title': Title,
-                'portal_type': getattr( o, 'portal_type', o.meta_type),
-                'folderish': o.isPrincipiaFolderish
+                'portal_type': getattr(o, 'portal_type', o.meta_type),
+                'folderish': o.isPrincipiaFolderish,
                 }
 
-    def cd( self, path ):
+    def cd(self, path):
         """
         Change current dir to a specific folder.
 
@@ -269,62 +277,82 @@ class ZopeDebug(object):
         if type(path) is not StringType:
             path = '/'.join(path.getPhysicalPath())
         cwd = self.pwd()
-        x = cwd.unrestrictedTraverse( path )
+        x = cwd.unrestrictedTraverse(path)
         if x is None:
-            raise KeyError( "Can't cd to %s" % path )
+            raise KeyError("Can't cd to %s" % path)
 
-        print "%s -> %s" % ( self.pwd().getId(), x.getId() )
+        print "%s -> %s" % (self.pwd().getId(), x.getId())
         self._pwd = x
 
-    def ls( self, x=None ):
+    def ls(self, x=None):
         """
         List object(s)
         """
         if type(x) is StringType:
             cwd = self.pwd()
-            x = cwd.unrestrictedTraverse( x )
+            x = cwd.unrestrictedTraverse(x)
         if x is None:
             x = self.pwd()
         if x.isPrincipiaFolderish:
             return [self.objectInfo(o) for id, o in x.objectItems()]
         else:
-            return self.objectInfo( x )
+            return self.objectInfo(x)
 
 zope_debug = None
 
+
 def ipy_set_trace():
-    import IPython; IPython.Debugger.Pdb().set_trace()
+    try:
+        from IPython import Debugger
+        Debugger.Pdb().set_trace()
+    except ImportError:
+        # IPython 0.10.2+
+        from IPython.core.debugger import Pdb
+        Pdb().set_trace()
+
 
 def main():
     global zope_debug
-    ip = ipapi.get()
+    ip = get_inst()
     # autocall to "full" mode (smart mode is default, I like full mode)
-    
-    SOFTWARE_HOME = os.environ.get( "SOFTWARE_HOME" )
-    sys.path.append( SOFTWARE_HOME )
-    print "SOFTWARE_HOME=%s\n" % SOFTWARE_HOME
+
+    SOFTWARE_HOME = os.environ.get("SOFTWARE_HOME")
+    if SOFTWARE_HOME:
+        sys.path.append(SOFTWARE_HOME)
+        print "SOFTWARE_HOME=%s\n" % SOFTWARE_HOME
+    else:
+        print "No $SOFTWARE_HOME set, assume Zope >= 2.12 (Plone 4 has this)."
 
     zope_debug = ZopeDebug()
 
     # <HACK ALERT>
-    import pdb;
+    import pdb
     pdb.set_trace = ipy_set_trace
     # </HACK ALERT>
 
-    print textwrap.dedent("""\
+    available_utils = ", ".join([
+        x for x in
+        dir(zope_debug.utils)
+        if not x.startswith("_")]
+    )
+    print textwrap.dedent("""
         ZOPE mode iPython shell.
 
           Bound names:
            app
            portal
-           utils.{ %s }
-  
-        Uses the $SOFTWARE_HOME and $CONFIG_FILE environment
-        variables.
-        """ % ( ",".join([ x for x in dir(zope_debug.utils) if not x.startswith("_") ] ) ) )
+           utils.{%s}
 
+        If you call utils.su() with no arguments, the PermissiveSecurityPolicy
+        will be put back in place.
 
-    ip.user_ns.update( zope_debug.namespace )
+        """ % available_utils)
+    if SOFTWARE_HOME:
+        print "Uses the $SOFTWARE_HOME and $CONFIG_FILE environment variables."
+    else:
+        print "Uses the $CONFIG_FILE environment variable."
+
+    ip.user_ns.update(zope_debug.namespace)
 
 
 main()
